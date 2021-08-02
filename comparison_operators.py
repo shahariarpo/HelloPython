735249c9c5485bf3a2305f8b4ca7350f769add7d
Shahariar Ahmed <shahariar.arpo@gmail.com>
2021-08-02 13:36:08 +0600

    update: comparison operators

---
+++ comparison_operators.py
@@ -0,0 +1,8 @@
+name = "Shahariar Ahmed"
+
+if len(name) < 3:
+    print("Name must be at least 3 character ")
+elif len(name) > 15:
+    print("Name must be a maximum of 15 characters")
+else:
+    print("Name looks good!")
