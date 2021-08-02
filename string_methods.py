7157f38a26d5400c961917bfa8b1e95a6d6880f5
Shahariar Ahmed <shahariar.arpo@gmail.com>
2021-08-02 12:14:26 +0600

    update: basic string methods

---
+++ string_methods.py
@@ -0,0 +1,7 @@
+course = "Python for beginners"
+print(len(course))
+print(course.upper())
+print(course.lower())
+print(course.replace("beginners", "absolute beginners"))
+print(course.find("o"))
+print("Python" in course)
