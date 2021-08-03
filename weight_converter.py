eabaaa5b2646ee6da20059dd25976104f2da247b
Shahariar Ahmed <shahariar.arpo@gmail.com>
2021-08-03 13:12:40 +0600

    update: basic while loops and a guessing game

--- weight_converter.py
+++ weight_converter.py
@@ -2,8 +2,8 @@ initial_weight = int(input("Your weight: "))
 unit = input("(L)bs or (K)g: ")
 
 if unit.upper() == "L":
-    converted = initial_weight * 0.45
+    converted = round(initial_weight * 0.45, 2)
     print(f'You are {converted} kilos')
 elif unit.upper() == "K":
-    converted = initial_weight / 0.45
+    converted = round(initial_weight / 0.45, 2)
     print(f'You are {converted} pounds')
