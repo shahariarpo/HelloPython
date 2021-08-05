fb80e3bcc6642a048da1c64f905b73094dfb6ea5
Shahariar Ahmed <shahariar.arpo@gmail.com>
2021-08-05 12:45:04 +0600

    update: organized code with reusable functions

--- emoji_converter.py
+++ emoji_converter.py
@@ -1,14 +1,19 @@
+def emoji_converter(message):
+    words = message.split(' ')
+    emoji_mapping = {
+        ":)": "🙂",
+        ":(": "😔",
+        ":P": "😛",
+        ":p": "😛",
+        "xd": "😆",
+        "XD": "😆"
+    }
+    output = ""
+    for word in words:
+        output += emoji_mapping.get(word, word) + " "
+    return output
+
+
 message = input(">")
-words = message.split(' ')
-emoji_mapping = {
-    ":)" : "🙂",
-    ":(" : "😔",
-    ":P" : "😛",
-    ":p" : "😛",
-    "xd" : "😆",
-    "XD" : "😆"
-}
-output = ""
-for word in words:
-    output += emoji_mapping.get(word, word) + " "
-print(output)
+results = emoji_converter(message)
+print(results)
